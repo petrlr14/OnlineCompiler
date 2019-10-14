f7958aa384de075e4e5226bedc8de8e4dbdccac7
Moemeister <00062716@uca.edu.sv>
2019-10-14 14:32:27 -0600

    manejo de errores

--- controllers/exec.js
+++ controllers/exec.js
@@ -5,13 +5,29 @@ module.exports = {
     exe: (req, res, next) => {
         const content = req.body.content;
         fs.writeFile('input.moe', content, (error) => {
-            console.log(error);
-            child = exec(`java -jar jar/spegmoe.jar input.moe`, (error, stdout, stder) => {
-                console.log(error);
+            if (error) {
+                return res.status(500).json({
+                    error1: error,
+                })
+            }
+            child = exec(`java -jar jar/spegmoe.jar input.moe`, (erro, stdout, stder) => {
+                if (erro) {
+                    return res.status(500).json({
+                        error1: erro,
+                    })
+                }
                 fs.readFile("output/output.txt", 'utf8', (err, data) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error1: err
+                        })
+                    }
                     fs.readFile("output/errors.txt", "utf8", (er, dat) => {
                         if (err) {
-                            console.log('error: ', err);
+                            return res.status(500).json({
+                                error1: err,
+                                error2: er
+                            })
                         } else {
                             console.log(data);
                             return res.status(200).json({
